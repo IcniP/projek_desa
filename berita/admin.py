df33bca1bae97d24ca1e18c0bbec15836e6b105f
Pinci <destructioner717@gmail.com>
2025-11-06 22:23:04 +0700

    mengganti tampilan admin django dengan jazzmin

--- berita/admin.py
+++ berita/admin.py
@@ -1,4 +1,22 @@
+# berita/admin.py
+
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Berita
 
-admin.site.register(Berita)
+@admin.register(Berita)
+class BeritaAdmin(admin.ModelAdmin):
+    list_display = ('judul', 'tampilkan_gambar', 'tanggal_publikasi')
+    list_filter = ('tanggal_publikasi',)
+    search_fields = ('judul',)
+    
+    def tampilkan_gambar(self, obj):
+        if obj.gambar:
+            return format_html(
+                '<a href="{0}" target="_blank">'
+                '<img src="{0}" style="max-height: 50px; border-radius: 4px;" />'
+                '</a>',
+                obj.gambar.url
+            )
+        return "Tidak Ada Foto"
+    tampilkan_gambar.short_description = 'Gambar'
--- profil/admin.py
+++ profil/admin.py
@@ -1,9 +1,48 @@
 # profil/admin.py
 
 from django.contrib import admin
-from .models import Profil, StrukturOrganisasi, LingkunganRT, Galeri 
+from django.utils.html import format_html
+from .models import Profil, StrukturOrganisasi, LingkunganRT, Galeri
 
-admin.site.register(Profil)
-admin.site.register(StrukturOrganisasi)
-admin.site.register(LingkunganRT)
-admin.site.register(Galeri)
+@admin.register(Profil)
+class ProfilAdmin(admin.ModelAdmin):
+    list_display = ('judul',)
+    search_fields = ('judul',)
+
+@admin.register(StrukturOrganisasi)
+class StrukturOrganisasiAdmin(admin.ModelAdmin):
+    list_display = ('nama', 'jabatan', 'nip', 'tampilkan_foto')
+    search_fields = ('nama', 'jabatan', 'nip')
+    
+    def tampilkan_foto(self, obj):
+        if obj.foto:
+            return format_html(
+                '<a href="{0}" target="_blank">'
+                '<img src="{0}" style="max-height: 50px; border-radius: 4px;" />'
+                '</a>',
+                obj.foto.url
+            )
+        return "Tidak Ada Foto"
+    tampilkan_foto.short_description = 'Foto'
+
+@admin.register(LingkunganRT)
+class LingkunganRTAdmin(admin.ModelAdmin):
+    list_display = ('nama_lingkungan', 'nomor_rt', 'nama_ketua_rt')
+    list_filter = ('nama_lingkungan',)
+    search_fields = ('nama_lingkungan', 'nomor_rt', 'nama_ketua_rt')
+
+@admin.register(Galeri)
+class GaleriAdmin(admin.ModelAdmin):
+    list_display = ('keterangan', 'tampilkan_gambar')
+    search_fields = ('keterangan',)
+    
+    def tampilkan_gambar(self, obj):
+        if obj.gambar:
+            return format_html(
+                '<a href="{0}" target="_blank">'
+                '<img src="{0}" style="max-height: 50px; border-radius: 4px;" />'
+                '</a>',
+                obj.gambar.url
+            )
+        return "Tidak Ada Foto"
+    tampilkan_gambar.short_description = 'Gambar'
