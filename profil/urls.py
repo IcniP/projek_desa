66151f184971f25e0043f8b83e967617e45be9d3
Pinci <destructioner717@gmail.com>
2025-11-09 13:38:16 +0700

    rapihin bagian profil desa dan umkm

--- profil/urls.py
+++ profil/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('profil-desa/', views.halaman_profil_lengkap, name='profil_lengkap'),
+    path('pelayanan/<int:kategori_id>/', views.layanan_detail, name='layanan_detail'),
     path('galeri/', views.galeri, name='galeri'),
     path('hubungi-kami/', views.hubungi_kami, name='hubungi_kami'),
 ]
